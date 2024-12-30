f5d6c52da4a8ee67317050d5a814a185246c9df4
Daniyal Ahmed <datariq0311@gmail.com>
2024-12-30 11:50:16 +0500

    next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
       },
     ],
   },
+  eslint: {
+    // Disable the warning for <img> tag usage in next/image
+    ignoreDuringBuilds: true, // This will ignore ESLint warnings during the build process
+  },
+  reactStrictMode: true, // Ensure React strict mode is enabled (optional but recommended)
+  // You can add other custom Next.js configurations here as well
 };
 
 module.exports = nextConfig;
